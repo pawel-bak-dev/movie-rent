11d671b0fc3da4ce39b1bfede888ddbe292d095d
pawel-bak-dev <pawel.bak.dev@gmail.com>
2021-01-11 01:56:30 +0100

    add authorization middleware

--- routes/genres.js
+++ routes/genres.js
@@ -2,6 +2,7 @@ const { Genre, validate } = require("../models/genre");
 const express = require("express");
 const router = express.Router();
 const auth = require("../middleware/auth");
+const admin = require("../middleware/admin");
 
 router.get("/", async (req, res) => {
   const genres = await Genre.find().sort("name");
@@ -36,7 +37,7 @@ router.put("/:id", auth, async (req, res) => {
   res.send(genre);
 });
 
-router.delete("/:id", auth, async (req, res) => {
+router.delete("/:id", [auth, admin], async (req, res) => {
   const genre = await Genre.findByIdAndRemove(req.params.id);
 
   if (!genre)
